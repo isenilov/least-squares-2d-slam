b9d842fde0839b85bb53d1e90714bede03bd3e69
isenilov <isenilov@gmail.com>
2018-01-28 13:51:47 +0300

    pose dim 3d->2d

--- least-squares-2d-slam/total_least_squares_indices.m
+++ least-squares-2d-slam/total_least_squares_indices.m
@@ -2,8 +2,8 @@
 # dimensions
 
 %(minimal) size of pose and landmarks
-global pose_dim=6;
-global landmark_dim=3;
+global pose_dim=3;
+global landmark_dim=2;
 
 
 # retrieves the index in the perturbation vector, that corresponds to
